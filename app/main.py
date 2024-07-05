518d649948181a89e61406a2ad22de4ce2c17a86
prevail <esehgodprevail@gmail.com>
2024-07-05 22:53:42 +0100

    changed few lines of code in the main.py and vercel.json

--- app/main.py
+++ app/main.py
@@ -63,4 +63,4 @@ async def get_info(request: Request, name: str = Query(..., description="your na
     return response
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
